f3d84fdf9a7581bd019699db1ef8abec684cac8e
Nícolas Iensen <nicolas.iensen@gmail.com>
2018-03-04 18:09:02 +0100

    Fetch, download and set the APOD as the wallpaper once the app is ready

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const electron = require("electron");
 const app = electron.app;
 
+const { APOD, setNasaApiKey } = require("nasa-sdk");
+const wallpaper = require("wallpaper");
+const download = require("download");
+
 app.on("ready", () => {
-  console.log("hello world!");
+  setNasaApiKey("DEMO_KEY");
+
+  APOD.fetch()
+    .then(apod => new Promise((resolve, reject) => resolve(apod["hdurl"])))
+    .then(
+      url =>
+        new Promise((resolve, reject) => {
+          download(url, "./images").then(() => resolve(url));
+        })
+    )
+    .then(url => wallpaper.set(`./images/${url.replace(/^.*[\\\/]/, "")}`))
+    .then(() => console.log("done"));
 });
