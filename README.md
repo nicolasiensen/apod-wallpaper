4b4848118be35a85b1e04d32e4c43c018fdc7474
Nícolas Iensen <nicolas.iensen@gmail.com>
2018-03-08 11:11:02 +0100

    Open the app as a tray icon

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # apod-wallpaper
+
 Astronomy Picture of the Day Wallpaper is a cross-platform desktop application that automatically changes your wallpaper to an amazing cosmic picture of the day ✨
+
+## Credits
+
+"[Telescope](https://thenounproject.com/term/telescope/389646/)" icon by [Creative Stall](http://www.creativestall.com/) from the [Noun Project](http://thenounproject.com/).
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const electron = require("electron");
-const app = electron.app;
-
+const menubar = require("menubar");
 const { APOD, setNasaApiKey } = require("nasa-sdk");
 const wallpaper = require("wallpaper");
 const download = require("download");
+
+const mb = menubar();
 
 const setWallpaper = () => {
   return APOD.fetch()
@@ -18,7 +18,7 @@ const setWallpaper = () => {
     .then(() => console.log("done"));
 };
 
-app.on("ready", () => {
+mb.on("ready", function ready() {
   setNasaApiKey(process.env.NASA_API_KEY);
   setWallpaper();
   setInterval(setWallpaper, 24 * 60 * 60 * 1000);
