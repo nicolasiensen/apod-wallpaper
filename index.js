4240c4d7923a4dad5601e1ba2ff3b309eea87cab
Nícolas Iensen <nicolas.iensen@gmail.com>
2018-04-22 11:02:05 +0200

    Use WallpaperManager to manage wallpapers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const menubar = require("menubar");
+const electron = require('electron');
 const { APOD, setNasaApiKey } = require("nasa-sdk");
-const wallpaper = require("wallpaper");
-const download = require("download");
 const path = require("path");
 const os = require("os");
+const WallpaperManager = require("./WallpaperManager");
 
 process.resourcesPath = process.env.PWD ? process.env.PWD : process.resourcesPath;
 
@@ -13,35 +13,13 @@ const mb = menubar({
   icon: path.join(process.resourcesPath, "build", "tray-icon.png")
 });
 
-const setWallpaper = (imagesPath, date) => {
-  return APOD.fetch({ date: `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}` })
-    .then(apod => new Promise((resolve, reject) => resolve(apod["hdurl"])))
-    .then(
-      url =>
-        new Promise((resolve, reject) => {
-          download(url, imagesPath).then(() => resolve(url));
-        })
-    )
-    .then(url => wallpaper.set(path.join(imagesPath, url.replace(/^.*[\\\/]/, ""))))
-    .then(() => console.log("done"));
-};
-
-const getToday = () => {
-  const today = new Date();
-  today.setHours(0,0,0,0);
-  return today;
-}
-
 mb.on("ready", function ready() {
   setNasaApiKey(process.env.NASA_API_KEY);
-  let wallpaperSetAt = getToday();
-  const imagesPath = path.join(os.tmpdir(), "com.nicolasiensen.apod-wallpaper")
-  setWallpaper(imagesPath, wallpaperSetAt);
+  const imagesPath = path.join(os.tmpdir(), "com.nicolasiensen.apod-wallpaper");
+  const wallpaperManager = new WallpaperManager(APOD, imagesPath);
 
-  setInterval(() => {
-    if (wallpaperSetAt.valueOf() !== getToday().valueOf()) {
-      wallpaperSetAt = getToday();
-      setWallpaper(imagesPath, wallpaperSetAt);
-    }
-  }, 10000);
+  wallpaperManager.setWallpaper();
+  wallpaperManager.enableDailyWallpaper();
+  electron.powerMonitor.on('suspend', wallpaperManager.disableDailyWallpaper)
+  electron.powerMonitor.on('resume', wallpaperManager.enableDailyWallpaper)
 });
