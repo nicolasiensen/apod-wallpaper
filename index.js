adeb12153bb0ab25b518600d5ffae3c054bf839c
Nícolas Iensen <nicolas.iensen@gmail.com>
2018-05-12 16:30:42 +0200

    Animate the tray icon when the wallpaper is being updated
    In this commit, we are using the new life cycle callbacks of the
    WallpaperManager to trigger the animation of the tray icon using the
    TrayIconManager.

--- index.js
+++ index.js
@@ -4,28 +4,49 @@ const { APOD, setNasaApiKey } = require("nasa-sdk");
 const path = require("path");
 const os = require("os");
 const WallpaperManager = require("./WallpaperManager");
+const TrayIconManager = require("./TrayIconManager");
 
 process.resourcesPath = process.env.PWD ? process.env.PWD : process.resourcesPath;
 
 require('dotenv').config({ path: path.join(process.resourcesPath, ".env") })
 
-const mb = menubar({
-  icon: path.join(process.resourcesPath, "build", "tray-icon.png"),
-  preloadWindow: true
-});
+const mb = menubar({ preloadWindow: true });
+const trayIcon = path.join(process.resourcesPath, "build", `tray-icon.png`);
+
+const trayAnimationFrames = [
+  path.join(process.resourcesPath, "build", `tray-icon-loading-1.png`),
+  path.join(process.resourcesPath, "build", `tray-icon-loading-2.png`),
+  path.join(process.resourcesPath, "build", `tray-icon-loading-3.png`),
+  path.join(process.resourcesPath, "build", `tray-icon-loading-4.png`)
+]
 
 mb.on("ready", function ready() {
   setNasaApiKey(process.env.NASA_API_KEY);
   const imagesPath = path.join(os.tmpdir(), "com.nicolasiensen.apod-wallpaper");
+  const trayIconManager = new TrayIconManager(mb.tray, trayIcon, trayAnimationFrames);
 
-  const wallpaperManager = new WallpaperManager(
-    APOD,
-    imagesPath,
-    wallpaper => {
-      mb.window.webContents.send('onWallpaperSet', wallpaper);
-      mb.showWindow();
-    }
-  );
+  const onWallpaperUpdateStart = () => {
+    trayIconManager.startAnimation();
+  };
+
+  const onWallpaperUpdateComplete = wallpaper => {
+    trayIconManager.stopAnimation();
+    mb.window.webContents.send('onWallpaperSet', wallpaper);
+    mb.showWindow();
+  };
+
+  const onWallpaperUpdateFail = error => {
+    trayIconManager.stopAnimation();
+    console.log("WallpaperManager error: ", error);
+  };
+
+  const wallpaperManager = new WallpaperManager(APOD, imagesPath, {
+    onWallpaperUpdateStart,
+    onWallpaperUpdateComplete,
+    onWallpaperUpdateFail
+  });
+
+  trayIconManager.setIcon();
 
   wallpaperManager.setWallpaper();
   wallpaperManager.enableDailyWallpaper();
